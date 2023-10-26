d1837922a48f53ebd1f199f16e0f791f46896b92
keyforwork <keyvanforwork@gmail.com>
2023-10-25 22:15:02 -0700

    create function add()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "add.h"
 #define MY_NAME "Karim"
 
 int main() {
     std::cout << "Hello, " << MY_NAME << '\n';
+    std::cout << "Age is , " << add(4, 20) << '\n';
+    #if 0
+    std::cout << "Good";
+    #endif
     return 0;
 }
