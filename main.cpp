03e80e5361823cb35240f609dfb7392d67eefae2
keyforwork <keyvanforwork@gmail.com>
2023-11-09 15:26:59 +0330

    Square root with goto expired instruction

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-
-void printNumber(int x) {
-    std::cout << "Number is equal to: ";
-    switch(x){
-        case 0:
-            std::cout << "Zero";
-            break;
-        case 1:
-            std::cout << "One";
-            break;
-        default:
-            std::cout << "Unknown";
-            break;
-    }
-
-}
+#include <cmath>
 
 int main() {
-    printNumber(1);
+    double x{};
+    tryAgain:
+    std::cout << "Enter a non-negative number: ";
+    std::cin >> x;
+
+    if(x < 0.0)
+        goto tryAgain;
+
+    std::cout << "The square root " << x <<  " is equal to :  " << std::sqrt(x) << '\n';
+
     return 0;
 }
